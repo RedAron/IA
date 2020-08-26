9300b65c5d0e9911c6737c6899e75b145c47f84e
janer gonzalez <janer369@gmail.com>
2020-08-26 14:24:36 -0500

    utilizacion de busting

--- bagging.py
+++ bagging.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+
 if __name__=="__main__":
     dt_heart=pd.read_csv('./data/heart.csv')
     print(dt_heart['target'].describe())
@@ -20,9 +21,8 @@ if __name__=="__main__":
     print("="*64)
     print(accuracy_score(knn_pred,y_test))
 
-    bag_class=BaggingClassifier(base_estimator=KNeighborsClassifier(),n_estimators=50).fit(X_train,y_train)
+    bag_class=BaggingClassifier(base_estimator=KNeighborsClassifier(),n_estimators=110).fit(X_train,y_train)
     bag_pred=bag_class.predict(X_test)
     print("="*64)
     print(accuracy_score(bag_pred,y_test))
-
 
